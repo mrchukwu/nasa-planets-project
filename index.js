b91c0612b99c12893938eb5115f92905363e775f
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-08-07 23:14:41 +0100

    filtering habitable planets

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const {parse} = require("csv-parse");
 const fs = require("fs");
 
-let result = [];
+const habtabilePlanets = [];
+
+function isHabitablePlanet(planet){
+    return planet["koi_disposition"] === "CONFIRMED" && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11
+    && planet["koi_prad"] < 1.6;
+}
 
 fs.createReadStream("kepler_data.csv")
 .pipe(parse({
@@ -10,13 +15,15 @@ fs.createReadStream("kepler_data.csv")
     columns: true,
 })) 
 .on('data', (data) => {
-    result.push(data);
+    if (isHabitablePlanet(data)) {
+        habtabilePlanets.push(data);
+    }
 })
 .on("error", (err) => {
     console.error("Error reading file: ", err);
 })
 .on("end", () => {
-    console.log("File read started....");
-    console.log("File:", result);
+    console.log(habtabilePlanets.map(planet => planet["kepler_name"]));
+    console.log(`Data: ${habtabilePlanets.length} of planets found.`);
     console.log("File read complete.");
 })
